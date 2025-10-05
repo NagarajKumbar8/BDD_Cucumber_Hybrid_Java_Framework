59fd34ac44bc495ced9d2394d93ba2af467b2b42
Nagaraj_kumbar <nagarajkumbar2225@gmail.com>
2025-10-05 18:15:23 +0530

    changed code

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,6 +1,17 @@
 package runner;
 
-public class TestRunner {
-    // Empty class – no annotation needed
-}
+import org.junit.runner.RunWith;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/features",
+				 glue={"stepdefinitions","hooks"},
+				 publish=true,
+				 plugin={"pretty","html:target/CucumberReports/CucumberReport.html"})
+public class TestRunner {
+	
+	
+
+}
